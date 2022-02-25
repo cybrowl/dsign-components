90392f5387d8e1248fb5aa6178da2b43934f2a3c
cyberowl <cybrowl@protonmail.com>
2022-02-24 21:19:36 -0800

    add: button

---
+++ src/stories/Button.svelte
@@ -0,0 +1,44 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+
+    export let disabled = false;
+    export let primary = false;
+    export let size = 'medium';
+    export let label = '';
+
+    let mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+    const dispatch = createEventDispatcher();
+
+    function onClick(event) {
+        dispatch('click', event);
+    }
+</script>
+
+<button
+    type="button"
+    {disabled}
+    class={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+    on:click={onClick}
+>
+    {label}
+</button>
+
+<style>
+    .storybook-button {
+        @apply font-sans font-bold text-white rounded-md border-0 cursor-pointer inline-block;
+    }
+    .storybook-button--primary {
+        @apply bg-tulip-purple;
+    }
+    .storybook-button--primary:active {
+        @apply bg-primary-purple;
+    }
+    .storybook-button--secondary {
+        @apply text-gray-900 bg-transparent border-[1px];
+    }
+    .storybook-button--medium {
+        font-size: 16px;
+        padding: 12px 24px;
+    }
+</style>
