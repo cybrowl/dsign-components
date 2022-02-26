32a3092bc43f9beb114921825dc246cc78e046a6
cyberowl <cybrowl@protonmail.com>
2022-02-26 06:27:11 -0800

    add: modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,48 @@
+<script>
+    import {fade, fly} from 'svelte/transition';
+    import {createEventDispatcher} from 'svelte';
+    import Icon from './Icon.svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function onClick(event) {
+        dispatch('click', event);
+    }
+
+    function onClose(event) {
+        dispatch('click', event);
+    }
+</script>
+
+<div class="modalBackdrop" on:click={onClick} />
+<div class="modalLayout" in:fly={{y: 200, duration: 1000}} out:fade>
+    <div class="modal">
+        <div class="modalHeader">
+            <span class="modalHeaderClose" on:click={onClose}>
+                <Icon class="closeStandard" name="close_standard" width="20" height="20" />
+            </span>
+        </div>
+        <div class="modalBody" />
+    </div>
+</div>
+
+<style>
+    .modalBackdrop {
+        @apply fixed top-0 left-0 right-0 h-full w-full bg-backdrop opacity-90;
+    }
+    .modalLayout {
+        @apply fixed top-40 left-1/4 right-1/4;
+    }
+    .modal {
+        @apply bg-white rounded-lg shadow bg-dark-grey;
+    }
+    .modalHeader {
+        @apply flex float-right;
+    }
+    .modalHeaderClose {
+        @apply p-5 rounded-t;
+    }
+    .modalBody {
+        @apply relative h-96;
+    }
+</style>
---
+++ src/stories/Modal.stories.svelte
@@ -0,0 +1,25 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import Modal from '../components/Modal.svelte';
+</script>
+
+<Meta
+    title="Dsign/Modal"
+    component={Modal}
+    argTypes={{
+        onClick: {action: 'onClick'},
+        onClose: {action: 'onClose'}
+    }}
+/>
+
+<Template let:args>
+    <Modal {...args} on:click={args.onClick} />
+</Template>
+
+<Story
+    name="Default"
+    args={{
+        primary: true,
+        label: 'Modal'
+    }}
+/>
