a8a9a4809ec3d19fe8701cb91f357a90d7b3af00
cyberowl <cybrowl@protonmail.com>
2022-02-26 03:16:22 -0800

    add: icons

---
+++ src/stories/Icon.stories.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import Icon from '../components/Icon.svelte';
+</script>
+
+<Meta
+    title="Dsign/Icon"
+    component={Icon}
+    argTypes={{
+        name: {control: 'text'},
+        width: {control: 'text'},
+        height: {control: 'text'}
+    }}
+/>
+
+<Template let:args>
+    <Icon {...args} on:click={args.onClick} />
+</Template>
+
+<Story
+    name="close_standard"
+    args={{
+        name: 'close_standard',
+        class: 'closeStandard',
+        width: '20',
+        height: '20'
+    }}
+/>
