4267a7c96d683c0f5bcdcf73ef576502095c00cd
cyberowl <cybrowl@protonmail.com>
2023-05-06 05:25:01 -0700

    move: index to lib

--- index.js
+++
@@ -1,5 +0,0 @@
-// index.js
-export { default as Button } from "./src/lib/components/Button.svelte";
-export { default as Avatar } from "./src/lib/components/Avatar.svelte";
-export { default as Icon } from "./src/lib/components/Icon.svelte";
-export { default as Input } from "./src/lib/components/Input.svelte";
