a048ce211343532aabc7ab2cb2fbd0bea7d3b571
cyberowl <cybrowl@protonmail.com>
2023-05-06 05:49:01 -0700

    fix: export component

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 // index.js
-export {default as Button} from './components/Button.svelte';
 export {default as Avatar} from './components/Avatar.svelte';
+export {default as Button} from './components/Button.svelte';
+export {default as FavoriteCardEmpty} from './components/FavoriteCardEmpty.svelte';
 export {default as Icon} from './components/Icon.svelte';
 export {default as Input} from './components/Input.svelte';
