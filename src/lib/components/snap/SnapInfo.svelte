4bb2fcb195a63bdc2c92633c8b9cda77a02da4e6
cyberowl <cybrowl@protonmail.com>
2023-05-19 01:56:45 -0700

    refactor: SnapInfo.svelte to use lodash's get and capitalize functions
    This commit refactors the SnapInfo.svelte file to use lodash's get and capitalize functions instead of importing createEventDispatcher. It also updates the code to extract project information using get function, and capitalizes the username.

--- src/lib/components/snap/SnapInfo.svelte
+++ src/lib/components/snap/SnapInfo.svelte
@@ -1,46 +1,36 @@
 <script>
 	import Button from '../basic_elements/Button.svelte';
 	import {DateTime} from 'luxon';
-	import {createEventDispatcher} from 'svelte';
-	import get from 'lodash/get';
-
-	const dispatch = createEventDispatcher();
+	import {get, capitalize} from 'lodash';
 
 	export let snap = {};
 
-	let created = DateTime.fromMillis(
+	let snap_name = get(snap, 'title', '');
+	let username = capitalize(get(snap, 'username', ''));
+	let published = DateTime.fromMillis(
 		Number(snap.created) / 1000000
 	).toLocaleString(DateTime.DATETIME_MED);
 
-	function saveToFavorites() {
-		project.metrics.likes = Number(project.metrics.likes) + 1;
-
-		project.likeActive = true;
-
-		dispatch('saveToFavorites', project);
-	}
+	let project_id = get(snap, 'project.id', '');
+	let project_name = get(snap, 'project.name', '');
+	let project_canister_id = get(snap, 'project.canister_id', '');
+	let project_href = `/project/${project_id}/?canister_id=${project_canister_id}`;
 </script>
 
 <div class="info">
 	<span class="breadcrumb">
 		<a href={`/${get(snap, 'username', '')}`}>{get(snap, 'username', '')}</a>
 		<span>/</span>
-		{#if get(snap, 'project.id', '').length > 0}
-			<a
-				href={`/project/${get(snap, 'project.id', '')}/?canister_id=${get(
-					snap,
-					'project.canister_id',
-					''
-				)}`}>{get(snap, 'project.name', '')}</a
-			>
+		{#if project_id.length > 0}
+			<a href={project_href}>{project_name}</a>
 			<span>/</span>
 		{/if}
-		<p>{get(snap, 'title', '')}</p>
+		<p>{snap_name}</p>
 	</span>
-	<h1>{get(snap, 'title', '')}</h1>
+	<h1>{snap_name}</h1>
 	<div class="details">
-		<p><span>Creator: </span>{get(snap, 'username', '')}</p>
-		<p><span>Published: </span>{created}</p>
+		<p><span>Creator: </span>{username}</p>
+		<p><span>Published: </span>{published}</p>
 	</div>
 	<div class="tags">
 		{#each get(snap, 'tags', []) as tag}
